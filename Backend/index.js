f5a0e8d4541df47fb5dde8718bcab3879dd90a12
sagarika sahoo <sagarikasahoo16@gmail.com>
2024-05-12 23:21:26 +0530

    cleaned the code by using router and contoller

--- Backend/index.js
+++ Backend/index.js
@@ -1,56 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose")
-const User = require("./schema/user.schema")
+const cors = require("cors");
+const SignupRoute = require("./src/routes/signup-route");
+const LoginRoute = require("./src/routes/login-route");
+const ProductRoute = require("./src/routes/products-route");
 
 const app = express();
 app.use(express.json())
+app.use(cors())
 const PORT = 3000;
 
 
 //singnup
-app.post("/signup", async (req, res) => {
-    const {first_name, last_name, username, email, password} = req.body;
-    try{
-        const existingUser = await User.findOne({$or: [{email}, {username}]});
-        if(existingUser){
-            return res.send({error: "User with this email or username already exists"});
-        }
-        const newUser = new User({first_name, last_name, username, email, password});
-        await newUser.save();
-        res.send({ message: "User created successfully"})
-    }
-    catch(error){
-        res.send(error.message)
-    }
-});
-
+app.use("/signup", SignupRoute)
 
 //Login
-app.post("/login", async (req, res)=> {
-    const {email, password} = req.body;
-    let exits = false;
-    let found_user = null;
-    try{
-        const user = await User.findOne({email});
-        if(!user || user.password !== password){
-            return res.send({
-                user_varified:exits,
-                user_details: found_user,
-                error: "Invalid email or passwrord"
-            })
-        }
-        exits = true;
-        found_user = user;
-        res.send({
-            user_varified:exits,
-            user_details: found_user,
-            message: "Login Successfully"
-        })
+app.use("/login", LoginRoute)
 
-    }catch(error){
-        res.send(error.message)
-    }
-})
+//All products
+app.use("/products", ProductRoute)
+
 
 
 
