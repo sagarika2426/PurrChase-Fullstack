c9a5f7b5a71055f1a1d9bf5e613e2a208486265c
sagarika sahoo <sagarikasahoo16@gmail.com>
2024-05-13 23:50:22 +0530

    header

---
+++ Frontend/Frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+    return(
+        <div className="bg-orange-800 h-20 mb-2 flex items-center justify-items-center p-2">
+    
+            <img src="https://us.123rf.com/450wm/zhanna26/zhanna261709/zhanna26170900035/85712562-black-silhouette-of-cat-vector-illustration.jpg?ver=6"
+            className="h-16 rounded-full"/>
+            <p className="text-white font-semibold text-xl ml-2">PurrChase</p>
+
+        </div>
+    )
+}
+
+export default Header;
