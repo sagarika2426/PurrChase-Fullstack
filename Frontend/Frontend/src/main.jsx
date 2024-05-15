018c0f0e1e30f2316b34619b5c6259cc9a106e18
sagarika sahoo <sagarikasahoo16@gmail.com>
2024-05-15 23:46:02 +0530

    added browser router and provider

--- Frontend/Frontend/src/main.jsx
+++ Frontend/Frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { Provider } from "react-redux";
+import Header from "./components/Header.jsx";
+import App from "./App.jsx";
+import Footer from "./components/Footer.jsx";
+import store from "./cart/store.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Header />
+      <App />
+      <Footer />
+    </BrowserRouter>
+  </Provider>
+);
