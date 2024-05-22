70ffc2ad54f19dd798da77255434b6d3ae4f1dfd
sagarika sahoo <sagarikasahoo16@gmail.com>
2024-05-22 23:20:18 +0530

    post route

--- Backend/src/routes/products-route.js
+++ Backend/src/routes/products-route.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const {products_get, product_get_by_id} = require("../controllers/products.controller");
+const {products_get, product_get_by_id, product_post} = require("../controllers/products.controller");
 
 const ProductRoute = express.Router();
 
 ProductRoute.get("/", products_get);
 ProductRoute.get("/:_id", product_get_by_id);
+ProductRoute.post("/", product_post); 
 
 module.exports = ProductRoute;
