76e3d2398201e6bee91fa6ce2ed909bd0603d625
sagarika sahoo <sagarikasahoo16@gmail.com>
2024-06-07 23:55:24 +0530

    addtoFav post endpoint

--- Backend/src/controllers/products.controller.js
+++ Backend/src/controllers/products.controller.js
@@ -1,44 +1,59 @@
 const Product = require("../schema/product.schema");
 const User = require("../schema/user.schema");
 
-const products_get = async(req, res) => {
-    try{
-        const products = await Product.find({});
-        res.send(products);
-    }catch(error){
-        res.send(error.message)
+const products_get = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.send(products);
+  } catch (error) {
+    res.send(error.message);
+  }
+};
+
+const product_get_by_id = async (req, res) => {
+  try {
+    const productId = req.params._id;
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.send("Product not found");
     }
+    res.send(product);
+  } catch (error) {
+    res.send(error.message);
+  }
+};
 
-}
+const product_post = async (req, res) => {
+  try {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.send(newProduct);
+  } catch (error) {
+    res.send(error.message);
+  }
+};
 
-const product_get_by_id = async(req, res) => {
-    try{
-        const productId = req.params._id;
-        const product = await Product.findById(productId);
-
-        if(!product){
-            return res.send("Product not found");
-        }
-        res.send(product);
-    }catch (error) {
-        res.send(error.message);
+const addToFav = async (req, res) => {
+  try {
+    const productId = req.params._id;
+    const userId = req.userId;
+    const user = await User.findById(userId);
+    if (!user) {
+        return res.status(404).send("User not found");
+      }
+      user.favorites.push(productId);
+      await user.save();
+  
+      res.send("Product added to favorites successfully");
+    } catch (error) {
+      res.status(500).send(error.message);
     }
-}
-
-
-const product_post = async(req, res) => {
-    try{
-        const newProduct = new Product(req.body);
-        await newProduct.save();
-        res.send(newProduct);
-    }catch(error){
-        res.send(error.message)
-    }
-}
-
+};
 
 module.exports = {
-    products_get,
-    product_get_by_id,
-    product_post
-}
+  products_get,
+  product_get_by_id,
+  product_post,
+  addToFav
+};
