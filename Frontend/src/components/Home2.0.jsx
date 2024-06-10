16d0188dd8b1e85797a58e6244a43ff4ff04abd3
sagarika sahoo <sagarikasahoo16@gmail.com>
2024-06-10 17:18:56 +0530

    ui update for small screen

--- Frontend/src/components/Home2.0.jsx
+++ Frontend/src/components/Home2.0.jsx
@@ -33,8 +33,8 @@ const HomeNew = function () {
       <section className="bg-orange-100 py-8 md:py-12">
         <div className="container mx-auto flex flex-col md:flex-row items-center">
           <div className="md:w-1/2 px-6 text-center md:text-left mb-8 md:mb-0">
-            <h1 className="font-bold text-3xl md:text-5xl mb-4 md:mb-6">Welcome to Our Pet Store!</h1>
-            <p className="text-lg md:text-xl">Find everything your cat needs from food to toys.</p>
+            <h1 className="font-bold text-2xl md:text-4xl mb-4 md:mb-6">Welcome to Our Pet Store!</h1>
+            <p className="text-md md:text-xl">Find everything your cat needs from food to toys.</p>
             <Link to={"/products"} className="inline-block mt-6 px-8 py-3 bg-blue-500 text-white rounded-full transition duration-300 hover:bg-blue-600">
               Shop Now
             </Link>
@@ -49,30 +49,30 @@ const HomeNew = function () {
       <section className="py-12">
         <div className="container mx-auto">
           <h2 className="font-bold text-2xl md:text-3xl mb-6 text-center">Popular Categories</h2>
-          <div className="grid grid-cols-3 md:grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
             {/* Category 1 */}
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center px-8">
               <Link to="/products/category/Food" className="block mb-4">
-                <img src="https://mypetz.co.in/wp-content/uploads/2023/04/3.jpg" alt="Food" className="w-36 lg:w-64 mx-auto rounded-lg shadow-lg hover:shadow-xl transition duration-300 p-4" />
+                <img src="https://mypetz.co.in/wp-content/uploads/2023/04/3.jpg" alt="Food" className="w-56 lg:w-64 mx-auto rounded-lg shadow-lg hover:shadow-xl transition duration-300 p-4" />
               </Link>
-              <h3 className="font-semibold lg:text-lg mb-2 text-md">Food</h3>
-              <p className="lg:text-sm text-center text-red-700 text-xs">Nutritious and delicious meals for your feline friend.</p>
+              <h3 className="font-semibold text-lg mb-2">Food</h3>
+              <p className="text-md text-center text-red-700">Nutritious and delicious meals for your feline friend.</p>
             </div>
             {/* Category 2 */}
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center px-8">
               <Link to="/products/category/Liter%20Supplies" className="block mb-4">
-                <img src="https://mypetz.co.in/wp-content/uploads/2023/04/8.jpg" alt="Litter Supplies" className="w-36 lg:w-64 mx-auto rounded-lg shadow-lg hover:shadow-xl transition duration-300 p-4" />
+                <img src="https://mypetz.co.in/wp-content/uploads/2023/04/8.jpg" alt="Litter Supplies" className="w-56 lg:w-64 mx-auto rounded-lg shadow-lg hover:shadow-xl transition duration-300 p-4" />
               </Link>
-              <h3 className="font-semibold lg:text-lg mb-2 text-md">Hygine</h3>
-              <p className="lg:text-sm text-center text-red-700 text-xs">Essentials for keeping your {"cat's"} litter box clean and fresh.</p>
+              <h3 className="font-semibold text-lg mb-2">Hygine</h3>
+              <p className="text-md text-center text-red-700">Essentials for keeping your {"cat's"} litter box clean and fresh.</p>
             </div>
             {/* Category 3 */}
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center px-8">
               <Link to="/products/category/Toys" className="block mb-4">
-                <img src="https://mypetz.co.in/wp-content/uploads/2023/04/11.jpg" alt="Toys" className="w-36 lg:w-64 mx-auto rounded-lg shadow-lg hover:shadow-xl transition duration-300 p-4" />
+                <img src="https://mypetz.co.in/wp-content/uploads/2023/04/11.jpg" alt="Toys" className="w-56 lg:w-64 mx-auto rounded-lg shadow-lg hover:shadow-xl transition duration-300 p-4" />
               </Link>
-              <h3 className="font-semibold lg:text-lg mb-2 text-md">Toys and Play</h3>
-              <p className="lg:text-sm text-center text-red-700 text-xs">Keep your cat entertained and happy with our selection of toys.</p>
+              <h3 className="font-semibold text-lg mb-2">Toys and Play</h3>
+              <p className="text-md text-center text-red-700">Keep your cat entertained and happy with our selection of toys.</p>
             </div>
           </div>
         </div>
@@ -82,20 +82,20 @@ const HomeNew = function () {
       <section className="lg:py-8 bg-gray-50">
         <div className="px-4 lg:px-6 m-auto">
           <h2 className="font-bold text-2xl mb-4 text-center">Featured Products</h2>
-          <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-6 gap-4">
+          <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-6 gap-4 text-center">
             {featuredProducts.map((product) => (
               <div key={product._id} className="border rounded-lg shadow-md bg-white transform transition-transform hover:scale-105 mb-4">
-              <img src={product.img[0]} alt={product.name} className="w-full h-52 object-cover" />
+              <img src={product.img[0]} alt={product.name} className="w-auto" />
               <div className="p-4">
                 <div>
                   <h3 className="font-semibold lg:text-lg mb-2 text-md">{product.name}</h3>
-                  <p className="text-gray-600 mt-2">₹{product.price}</p>
+                  <p className="text-red-800 mt-2 font-semibold">₹{product.price}</p>
                 </div>
-                <div className="flex items-center">
+                <div className="font-bold">
                   <span>{product.ratings}</span>
                 </div>
               </div>
-              <div className="p-4">
+              <div className="p-2 text-sm lg:text-md">
                 <Link to={`/products/${product._id}`} className="block bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 text-center hover:bg-blue-600">
                   View Product
                 </Link>
@@ -109,7 +109,7 @@ const HomeNew = function () {
       </section>
 
       {/* What We Offer Section */}
-      <section className="py-12 px-2">
+      <section className="py-12 px-4">
         <div className="container mx-auto">
           <h2 className="font-bold text-2xl md:text-3xl mb-8 text-center">What We Offer</h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
